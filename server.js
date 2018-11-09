dbc71660b52e630e31f72e6c2e17b1936fc69757
Mac User <BradleyMitchellis@gmail.com>
2018-11-09 12:51:24 -0500

    deployment fix

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('${__dirname}/bar-beer-drinker-app/dist/'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
-app.get("/", function(req, res){
-    res.sendFile("index.html", {"root": __dirname});
-});
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'dist', 'index.html');
+    res.sendFile(index);
+  });
 
 console.log('Console Listening')
