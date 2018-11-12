5bc5dd28915d2189af58c90b382d8715d0e7d00a
Mac User <BradleyMitchellis@gmail.com>
2018-11-12 18:15:44 -0500

    add mySQL

--- db_connection.js
+++ db_connection.js
@@ -1,6 +1,6 @@
-/*var mysql = require('mysql');
+var mysql = require('mysql');
 
-var con = mysql.createConnection({
+/*var con = mysql.createConnection({
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "b2bfee3d6276cf",
     password: "4afb5aa1"
