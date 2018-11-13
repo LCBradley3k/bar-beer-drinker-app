ea2f67186539dadd325deb5a0581dcfcf8e128ce
Michal Kobylarz <kobylarzm@msn.com>
2018-11-13 16:32:48 -0500

    Inserted graph for Drinker analysis

--- src/app/drinker/drinker.component.ts
+++ src/app/drinker/drinker.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvasjs.min';
+
 
 @Component({
   selector: 'app-drinker',
@@ -10,6 +12,79 @@ export class DrinkerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    let chart = new CanvasJS.Chart("beerOrderedMostChart", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Beers Ordered Most By Drinker"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+      
+    chart.render();
+
+    let chart2 = new CanvasJS.Chart("daysSpentMostChart", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Drinker Spending Habit by Day"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+      
+    chart2.render();
+
+    let chart3 = new CanvasJS.Chart("monthsSpentMostChart", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Drinker Spending Habit by Month"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+      
+    chart3.render();
+
   }
 
 }
